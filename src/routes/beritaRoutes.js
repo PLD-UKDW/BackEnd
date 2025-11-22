1e8060dbc529577eaab93a5b4b29fffb9e28e2f2
ameliaetsa <maria.ni@ti.ukdw.ac.id>
2025-11-22 17:02:55 +0700

    feat: refactor seed data and enhance login functionality with OTP for admin; implement role-based access control in routes

--- src/routes/beritaRoutes.js
+++ src/routes/beritaRoutes.js
@@ -1,20 +1,30 @@
+// const express = require('express');
+// const router = express.Router();
+// const upload = require('../middleware/multer');
+// const beritaController = require('../controller/beritaController');
+// const auth = require('../middleware/auth');
+// const isAdmin = require('../middleware/isAdmin');
+
+// router.post('/api/create-berita', upload.single('content_images'), beritaController.createBerita);
+// router.get("/api/berita-admin", beritaController.getBeritaAdmin);
+// router.get("/api/berita-admin/:id", beritaController.getBeritaByIdAdmin);
+// router.put("/api/publish-berita/:id", beritaController.publishBerita);
+// router.put("/api/unpublish-berita/:id", beritaController.unpublishBerita);
+// router.put('/api/update-berita/:id', upload.single('content_images'), beritaController.updateBerita);
+// router.delete("/api/delete-berita/:id", beritaController.deleteBerita);
+
+// router.get("/api/berita-public", beritaController.getBeritaPublic);
+// router.get("/api/berita-public/:id", beritaController.getBeritaByIdPublic);
+
+
+// module.exports = router;
+
+
 const express = require('express');
 const router = express.Router();
-const upload = require('../middleware/multer');
-const beritaController = require('../controller/beritaController');
-const auth = require('../middleware/auth');
-const isAdmin = require('../middleware/isAdmin');
+const login = require('../controllers/loginController');
 
-router.post('/api/create-berita', upload.single('content_images'), beritaController.createBerita);
-router.get("/api/berita-admin", beritaController.getBeritaAdmin);
-router.get("/api/berita-admin/:id", beritaController.getBeritaByIdAdmin);
-router.put("/api/publish-berita/:id", beritaController.publishBerita);
-router.put("/api/unpublish-berita/:id", beritaController.unpublishBerita);
-router.put('/api/update-berita/:id', upload.single('content_images'), beritaController.updateBerita);
-router.delete("/api/delete-berita/:id", beritaController.deleteBerita);
-
-router.get("/api/berita-public", beritaController.getBeritaPublic);
-router.get("/api/berita-public/:id", beritaController.getBeritaByIdPublic);
-
+router.post('/login', login.login);
+router.post('/verify-admin', login.verifyAdmin);
 
 module.exports = router;
